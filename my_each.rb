ff365a41358bb534d06ceee9cb4c3e7c408dc2eb
Samantha Meason <sbmeason@gmail.com>
2020-02-01 00:27:43 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+words = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(array)
+  if block_given?
+  i = 0 
+  while i < array.length 
+  yield array[i]
+  i = i + 1 
+  end
+  return array 
+else
+  raise "This block should not run!"
+end
 end
